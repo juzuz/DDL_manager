4786f5970ca1d8e44c41d75da8bb1ab2e2130b0b
VongolaTeong <0522teongkarchun@gmail.com>
2021-05-07 09:24:23 +0800

    Create reward.js
    basic formula for reward calculation

---
+++ src/components/reward/reward.js
@@ -0,0 +1,13 @@
+import moment from 'moment-timezone';
+import firestore from '@react-native-firebase/firestore';
+
+//function to calculate reward
+//input: completed task from database
+function reward(data){
+	//placeholders
+	var a = 0.5;
+	var b = 0.5;
+
+	var completeTime = firestore.Timestamp.fromDate(moment().toDate());
+	return a * data.importanceScore + b * (completeTime - data.startTime) / (data.ddl - data.startTime);
+}
