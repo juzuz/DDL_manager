9df87eb8174d3d27596a5f342dab10508f2f9670
juzuz <juzu1052@gmail.com>
2021-05-09 13:37:52 -0400

    bug fix

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -13,23 +13,28 @@ const Drawer = createDrawerNavigator();
 export default function AppStack(props) {
 
     const {logout} = useContext(AuthContext);
-    const [trackingDate, setTrackingDate] = useState();
+    const [trackingDate, setTrackingDate] = useState(null);
 
     useEffect(() => {
         let statRef = firestore().collection('stats').doc(props.user);
         statRef.get().then(doc=>{
-            if (!doc.exists){
+            if(doc.exists){
+                setTrackingDate(doc.data().trackingDate.toDate())
+            }
+            if (!doc.exists){   
+                let now =moment();
+                
                 statRef.set({
                     completeHistory:1,
                     completedTask:0,
                     incompletedTask:0,
                     reward:0,
-                    trackingDate:firestore.Timestamp.fromDate(moment().toDate())
+                    trackingDate:firestore.Timestamp.fromDate(now.toDate())
                 })
+                setTrackingDate(now.toDate());
+
             }
-            else{
-                setTrackingDate(doc.data().trackingDate)
-            }
+           
         })
     }, [])
 
@@ -46,7 +51,7 @@ export default function AppStack(props) {
     }}>
         <Drawer.Screen name="Task Manager" component={HomeStack} initialParams={{user: props.user}} />
         <Drawer.Screen name='Your Stats' component={StatScreen} initialParams={{user:props.user, trackingDate:trackingDate}} />
-	<Drawer.Screen name='Shop' component={ShopScreen} initialParams={{user:props.user}} />
+	    <Drawer.Screen name='Shop' component={ShopScreen} initialParams={{user:props.user}} />
     </Drawer.Navigator>
     :null
     }
