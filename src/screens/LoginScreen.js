18f6fe4e9dbc42e3f89f952f028040dbf06169ba
juzuz <juzu1052@gmail.com>
2021-05-10 01:14:58 -0400

    Login screen css

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,12 @@
 import React, { useContext, useState, useEffect} from 'react';
-import {Text,View,StyleSheet,StatusBar, Dimensions, Alert} from 'react-native';
-import {Icon,Button,Container,Content,Form,Item,Input, Title} from 'native-base';
+import {Text,View,StyleSheet,StatusBar, Dimensions, Image,Alert,TextInput,TouchableOpacity} from 'react-native';
+import {Icon,Button,Container,Content,Item,} from 'native-base';
 import { AuthContext } from '../navigation/AuthProvider';
 import firestore from '@react-native-firebase/firestore';
 import firebase from '@react-native-firebase/app'
+import * as Animatable from 'react-native-animatable'
+import LinearGradient from 'react-native-linear-gradient';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -74,47 +77,83 @@ export default function LoginScreen(props) {
         <>
         <StatusBar hidden={true}></StatusBar>
         <Container style={styles.container}>
-            
+            <View style= {{alignItems:'center'}}>
+            <Animatable.Image 
+            animation="bounceIn"
+            source={require('../assets/logo.png')} 
+            style={{width:250,height:250}}
+            resizeMode = 'stretch'
+            />
+            </View>
             <Content contentContainerStyle={{
-                margin:20,
-                justifyContent:'center'
+                flex:1,
+                marginTop:10
             }}>
-                <Form style ={styles.form}>
-                    <Title style ={{alignSelf:'center'}}>
-                        DDL MANAGER
-                    </Title>
-                    <Item style={{marginLeft:0, borderBottomWidth:0}}> 
-                        <Input 
-                            placeholder="Email" 
-                            style={styles.input} 
-                            onChangeText={emailHandler}
-                            />
-                    </Item>
-                    <Item style={{marginLeft:0,borderBottomWidth:0}}> 
-                        <Input 
-                            placeholder="Password" 
-                            style={styles.input} 
-                            onChangeText={passwordHandler}
-                            secureTextEntry={true}
-                            />
-                    </Item>
-                    <Item style={{marginLeft:0,borderBottomWidth:0}}>
-                        <Button rounded info 
-                        style ={styles.button}
-                        onPress={loginHandler}
+                   <View style={styles.form}>
+                       <View style={{height:50,marginTop:30,marginLeft:20,paddingRight:25}}>
+                           <Text style={{fontSize:16,fontWeight:'bold',color:"#00e8dc"}}>Email</Text>
+                           <View style={styles.action}>
+                                <FontAwesome 
+                                    name="user"
+                                    color={'#00e8dc'}
+                                    size={26}
+                                />
+                               <TextInput placeholder="Your Email"
+                               placeholderTextColor={'white'}
+
+                               style={styles.textInput}
+                               onChangeText={emailHandler}
+                               />
+                           </View>
+                       </View>
+
+                       <View style={{height:50,marginTop:30,marginLeft:20,paddingRight:25}}>
+                           <Text style={{fontSize:16,fontWeight:'bold',color:"#00e8dc"}}>Password</Text>
+                           <View style={styles.action}>
+                           <FontAwesome 
+                                    name="lock"
+                                    color={'#00e8dc'}
+                                    size={30}
+                                />
+                               <TextInput placeholder="Your Password"
+                               secureTextEntry={true}
+                               placeholderTextColor={'white'}
+                               style={styles.textInput}
+                               onChangeText={passwordHandler}
+                               />
+                           </View>
+                       </View>
+                   
+                    <View style={{marginTop:40,marginLeft:20,marginRight:20}}>
+                    <TouchableOpacity
+                    style={styles.signIn}
+                    onPress={loginHandler}
+                     >
+                        <LinearGradient
+                        colors={['#00e8dc', '#00b3a9']}
+                        style={styles.signIn}
                         >
-                            <Text style ={styles.text}>Login</Text>
-                        </Button>
-                    </Item>
-                    <Item style={{marginLeft:0,borderBottomWidth:0}}>
-                        <Button rounded warning 
-                        style ={styles.button}
+                            <Text style={[styles.textSign, {
+                                color:'#fff'
+                            }]}>Sign In</Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                      style={[styles.signIn, {
+                        borderColor: '#009387',
+                        borderWidth: 1,
+                        marginTop: 15
+                    }]}
                         onPress={registerHandler}
-                        >
-                            <Text style ={styles.text}>Register</Text>
-                        </Button>
-                    </Item>
-                </Form>
+                     >
+                        <Text style={[styles.textSign, {
+                            color:'white'
+                        }]}>Sign Up</Text>
+                    </TouchableOpacity>
+                    </View>
+                  
+                    </View>
             </Content>
         </Container>
         </>
@@ -127,17 +166,21 @@ const styles = StyleSheet.create({
         backgroundColor:'#1e212a',
     },
     form:{
-        marginTop:windowHeight*0.3,
-        backgroundColor:'#1e212a'
+        flex:1,
+        flexDirection:'column',
+        backgroundColor:'white',
+        borderRadius:25,
+        backgroundColor:'#33353d'
+        // alignItems:'center'
     },
     input:{
-        backgroundColor:"#e5e5f3",
-        marginTop:10,
+        // backgroundColor:"#e5e5f3",
+        marginTop:20,
         borderRadius:5,
-        height:60,
         color:'black',
-        textAlign:'center',
-        fontSize:20,
+        textAlign:'left',
+        fontSize:18,
+        borderBottomWidth:1,
     }, 
     button:{
         flex:1,
@@ -147,5 +190,29 @@ const styles = StyleSheet.create({
         fontSize:18,
         fontWeight:'bold',
         color:'#E5E5F3'
+    },
+    action: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+        paddingBottom: 5
+    },
+    textInput: {
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? 0 : -12,
+        paddingLeft: 10,
+        color: 'white',
+    },
+    signIn: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
+    },
+    textSign: {
+        fontSize: 18,
+        fontWeight: 'bold'
     }
 })
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,8 +1,11 @@
 import React, { useContext, useState } from 'react';
-import {Text,View,StyleSheet,StatusBar, Dimensions, Alert} from 'react-native';
-import {Icon,Button,Container,Content,Form,Item,Input, Title} from 'native-base';
+import {Text,View,StyleSheet,StatusBar, Dimensions,TextInput,TouchableOpacity} from 'react-native';
+import {Icon,Button,Container,Content,Form,Item, } from 'native-base';
 import { AuthContext } from '../navigation/AuthProvider';
-import firebase from '@react-native-firebase/app'
+import firestore from '@react-native-firebase/firestore';
+import * as Animatable from 'react-native-animatable'
+import LinearGradient from 'react-native-linear-gradient';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -28,99 +31,101 @@ export default function RegisterScreen(props) {
     }
 
     const registerHandler = () =>{
-	var isValid = false;
-
-	if (email === ""){
-		Alert.alert('Alert', 'Email address must not be empty');	
-	}
-	else if (password === ""){
-		Alert.alert('Alert', 'Password must not be empty');
-	}
-
-	else if (confPass === "") {
-		Alert.alert('Alert', 'Confirm password must not be empty');
-	}
-
-	else if (confPass !== password) {
-		Alert.alert('Alert', 'Confirm password and password must be the same');
-	}
-	//validate email
-	else {	
-		firebase.auth().fetchSignInMethodsForEmail(email).then(function(user) {
-		}).catch(function(error) {
-    			var errorCode = error.code;
-    			var errorMessage = error.message;
-
-    			if (errorCode === 'auth/invalid-email') {
-        			alert('Invalid email.');
-    			} else {
-        			alert(errorMessage);         
-    			}
-    			console.log(error);
-		});
-		isValid = true;
-	}
-	if (isValid){
-		firebase.auth().createUserWithEmailAndPassword(email,password).then(function(user) {
-		}).catch(function(error) {
-    			var errorCode = error.code;
-    			var errorMessage = error.message;
-
-    			if (errorCode === 'auth/weak-password') {
-        			alert('Password should be at least 6 characters.');
-    			} else if (errorCode === 'auth/email-already-in-use') {
-        			alert("The email address is already in use by another account.");         
-    			}
-			else {
-        			alert(errorMessage);         
-    			}
-    			console.log(error);
-		});;
-	}
+        register(email,password);
+        
     }
     
     return (
         <>
         <StatusBar hidden={true}></StatusBar>
         <Container style={styles.container}>
-            
-            <Content contentContainerStyle={{
-                margin:20,
-                justifyContent:'center'
+        <View style= {{alignItems:'center'}}>
+            <Animatable.Image 
+            animation="bounceIn"
+            source={require('../assets/logo.png')} 
+            style={{width:250,height:250}}
+            resizeMode = 'stretch'
+            />
+            </View>
+        <Content contentContainerStyle={{
+                flex:1,
+                marginTop:10
             }}>
-                <Form style ={styles.form}>
-                    
-                    <Item style={{marginLeft:0, borderBottomWidth:0}}> 
-                        <Input 
-                        placeholder="Type in your email" 
-                        style={styles.input} 
-                        onChangeText={emailHandler}/>
-                    </Item>
-                    <Item style={{marginLeft:0,borderBottomWidth:0}}> 
-                        <Input 
-                            placeholder="Enter your password" 
-                            style={styles.input} 
-                            onChangeText={passwordHandler}
-                            secureTextEntry={true}/>
-                    </Item>
-                    <Item style={{marginLeft:0,borderBottomWidth:0}}> 
-                        <Input 
-                            placeholder="Confirm your password" 
-                            style={styles.input} 
-                            onChangeText={passwordConfHandler}
-                            secureTextEntry={true}/>
-                    </Item>
+                   <View style={styles.form}>
+                       <View style={{height:50,marginTop:30,marginLeft:20,paddingRight:25}}>
+                           <Text style={{fontSize:16,fontWeight:'bold',color:"#00e8dc"}}>Email</Text>
+                           <View style={styles.action}>
+                                <FontAwesome 
+                                    name="user"
+                                    color={'#00e8dc'}
+                                    size={26}
+                                />
+                               <TextInput placeholder="Your Email"
+                               placeholderTextColor={'white'}
+                               style={styles.textInput}
+                               onChangeText={emailHandler}
+                               />
+                           </View>
+                       </View>
+
+                       <View style={{height:50,marginTop:30,marginLeft:20,paddingRight:25}}>
+                           <Text style={{fontSize:16,fontWeight:'bold',color:"#00e8dc"}}>Password</Text>
+                           <View style={styles.action}>
+                           <FontAwesome 
+                                    name="lock"
+                                    color={'#00e8dc'}
+                                    size={30}
+                                />
+                               <TextInput placeholder="Your Password"
+                                placeholderTextColor={'white'}
+
+                               secureTextEntry={true}
+                               style={styles.textInput}
+                               onChangeText={passwordHandler}
+                               />
+                           </View>
+                       </View>
+                       <View style={{height:50,marginTop:30,marginLeft:20,paddingRight:25}}>
+                           <Text style={{fontSize:16,fontWeight:'bold',color:"#00e8dc"}}>Confirm Password</Text>
+                           <View style={styles.action}>
+                           <FontAwesome 
+                                    name="lock"
+                                    color={'#00e8dc'}
+                                    size={30}
+                                />
+                               <TextInput placeholder="Confirm Your Password"
+                                placeholderTextColor={'white'}
+
+
+                               secureTextEntry={true}
+                               style={styles.textInput}
+                               onChangeText={passwordConfHandler}
+                               />
+                           </View>
+                       </View>
                    
-                    <Item style={{marginLeft:0,borderBottomWidth:0}}>
-                        <Button rounded warning 
-                        style ={styles.button}
-                        onPress={registerHandler}
+                    <View style={{marginTop:40,marginLeft:20,marginRight:20}}>
+                    
+
+                    <TouchableOpacity
+                    style={styles.signIn}
+                    onPress={registerHandler}
+                     >
+                        <LinearGradient
+                        colors={['#00e8dc', '#00b3a9']}
+                        style={styles.signIn}
                         >
-                            <Text style ={styles.text} >Register</Text>
-                        </Button>
-                    </Item>
-                </Form>
+                            <Text style={[styles.textSign, {
+                                color:'#fff'
+                            }]}>Sign Up</Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                    </View>
+                    
+                  
+                    </View>
             </Content>
+          
         </Container>
         </>
     )
@@ -132,17 +137,21 @@ const styles = StyleSheet.create({
         backgroundColor:'#1e212a',
     },
     form:{
-        marginTop:windowHeight*0.2,
-        backgroundColor:'#1e212a'
+        flex:1,
+        flexDirection:'column',
+        backgroundColor:'white',
+        borderRadius:25,
+        backgroundColor:'#33353d'
+        // alignItems:'center'
     },
     input:{
-        backgroundColor:"#e5e5f3",
-        marginTop:10,
+        // backgroundColor:"#e5e5f3",
+        marginTop:20,
         borderRadius:5,
-        height:60,
         color:'black',
-        textAlign:'center',
-        fontSize:20,
+        textAlign:'left',
+        fontSize:18,
+        borderBottomWidth:1,
     }, 
     button:{
         flex:1,
@@ -152,5 +161,29 @@ const styles = StyleSheet.create({
         fontSize:18,
         fontWeight:'bold',
         color:'#E5E5F3'
+    },
+    action: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+        paddingBottom: 5
+    },
+    textInput: {
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? 0 : -12,
+        paddingLeft: 10,
+        color: 'white',
+    },
+    signIn: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
+    },
+    textSign: {
+        fontSize: 18,
+        fontWeight: 'bold'
     }
 })
